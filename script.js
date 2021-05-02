b9cdcbc74ede1f185977acaabad923fc817fcd1c
gordon-zhao <gordon-zhao@users.noreply.github.com>
2021-05-03 03:33:36 +0800

    part 1 + 2

--- script.js
+++ script.js
@@ -4,13 +4,37 @@ const img = new Image(); // used to load image from <input> and draw to canvas
 
 // Fires whenever the img object loads a new image (such as with img.src =)
 img.addEventListener('load', () => {
-  // TODO
-
   // Some helpful tips:
   // - Fill the whole Canvas with black first to add borders on non-square images, then draw on top
   // - Clear the form when a new image is selected
   // - If you draw the image to canvas here, it will update as soon as a new image is selected
+  let canvas = document.getElementById('user-image');
+  if (canvas.getContext) {
+    let ctx = canvas.getContext('2d');
+    ctx.fillStyle = 'rgb(0,0,0)';
+    ctx.fillRect(0,0, canvas.width, canvas.height);
+
+    let img_dim = getDimmensions(canvas.width, canvas.height, img.width, img.height);
+    ctx.drawImage(img, img_dim.startX, img_dim.startY, img_dim.width, img_dim.height);
+  }
+  else {
+    console.log("Canvas unsupported!");
+  }
 });
+
+// Handle image input
+let image_input = document.getElementById('image-input');
+image_input.addEventListener('change', (event) => {
+  img.src = URL.createObjectURL(image_input.files[0]);
+});
+
+// Handle Generate button
+// So bad! Why leaving the id unset? >_>
+let generate = document.querySelector("#generate-meme button[type='submit']");
+generate.onclick = function(e) {
+  e.preventDefault();
+  let inputs = document.getElementById("")
+}
 
 /**
  * Takes in the dimensions of the canvas and the new image, then calculates the new
